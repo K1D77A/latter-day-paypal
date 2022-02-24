fc77635fb56695536744ff70230419abe25ed6a5
josh <k1llfrenzy@Hotmail.co.uk>
2022-02-24 11:07:00 +0200

    get-token no longer refreshes if token is not bound and not expired

--- api/token.lisp
+++ api/token.lisp
@@ -54,17 +54,19 @@
 
 (defun get-token ()
   (assert (member *parse-as* '(:hash-table :plist)))
-  (wrapped-dex-call (resp status)
-                    (dex:post (format nil "~A/v1/oauth2/token"
-                                      (generate-url t))
-                              :basic-auth `(,*client* . ,*secret*)
-                              :headers '(("Accept" . "application/json")
-                                         ("Accept-Language" . "en_US"))
-                              :content '(("grant_type" . "client_credentials")))
-                    (let ((token (parse-token *parse-as* (jojo:parse resp :as *parse-as*))))
-                      (values 
-                       (make-instance (determine-good-class status) :body (list token))
-                       (setf *token* token)))))
+  (when (or (not (boundp '*token*))
+            (expiredp *token*))
+    (wrapped-dex-call (resp status)
+      (dex:post (format nil "~A/v1/oauth2/token"
+                        (generate-url t))
+                :basic-auth `(,*client* . ,*secret*)
+                :headers '(("Accept" . "application/json")
+                           ("Accept-Language" . "en_US"))
+                :content '(("grant_type" . "client_credentials")))
+      (let ((token (parse-token *parse-as* (jojo:parse resp :as *parse-as*))))
+        (values 
+         (make-instance (determine-good-class status) :body (list token))
+         (setf *token* token))))))
 
 (defmethod expiredp (token)
   (error 'unbound-token))
